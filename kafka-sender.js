750a83bdd3c41549af4c76e1eaa832fdbb846b86
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-02-07 14:54:24 +0100

    Nachricht kann an lokalen Kafka-Server gesendet werden

--- kafka-sender.js
+++ kafka-sender.js
@@ -12,3 +12,24 @@ if (!process.env.KAFKA_TOPIC) {
 console.log("Versuche, Nachricht auf das folgende Kafka-Topic zu schreiben: " + process.env.KAFKA_TOPIC);
 
 
+// write message to Kafka topic
+const { Kafka } = require("kafkajs");
+
+const kafka = new Kafka({ brokers: [ "localhost:9092" ], clientId: "nodejs-kafka-sender" });
+
+const producer = kafka.producer();
+
+
+const asyncBlock = async () => {
+
+    await producer.connect();
+    await producer.send({
+                    topic: process.env.KAFKA_TOPIC,
+                    messages: [{ value: "Hallo KafkaJS!" }]
+    });
+
+    console.log("Nachricht wurde erfolgreich gesendet.");
+
+};
+
+asyncBlock().catch(console.error)
