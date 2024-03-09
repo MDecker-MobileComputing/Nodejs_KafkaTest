897f20a7074c79975a8a5b38ca97a4c2066239fd
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-03-09 10:47:16 +0100

    dazu: kafka-topic-liste.js

---
+++ src/kafka-topic-liste.js
@@ -0,0 +1,44 @@
+const { Kafka, logLevel } = require("kafkajs");
+
+
+const kafka = new Kafka({ brokers: [ "localhost:9092" ],
+                          clientId: "nodejs-kafka-liste",
+                          logLevel: logLevel.ERROR
+                        });
+
+/*
+const kafka = new Kafka({
+    clientId: 'nodejs-kafka-sender',
+    brokers: ['zimolong.eu:9092'],
+    sasl: {
+        mechanism: 'plain',
+        username: 'alice',
+        password: 'g3h3im'
+    },
+    ssl: false, // Disabling SSL as you're using SASL_PLAINTEXT
+    connectionTimeout: 1000,
+    authenticationTimeout: 1000,
+    logLevel: logLevel.ERROR,
+});
+*/
+
+
+const admin = kafka.admin();
+
+const run = async () => {
+
+    await admin.connect();
+
+    const stringArrayOfTopics = await admin.listTopics();
+
+    console.log(`\nAnzahl Topics gefunden: ${stringArrayOfTopics.length}\n`);
+
+    for (let topic of stringArrayOfTopics) {
+        console.log(`Topic: ${topic}`);
+    }
+    console.log();
+
+    await admin.disconnect();
+}
+
+run().catch( console.error );
